715ec7448ae2fdfdee7e2a020aa05d760ec6bbea
KatyBrown <katherine.browm@imm.ox.ac.uk>
2017-09-26 16:56:02 +0100

    refined to use with much bigger networks, added transmission type option

--- build_network.py
+++ build_network.py
@@ -3,7 +3,8 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-
+import random
+import matplotlib.patches as mpatches
 
 def shufflePositions(N, level):
     '''
@@ -37,15 +38,16 @@ def buildInitialNetwork(hostdict, scoredict):
     Nodes are coloured according to their host based on the
     file provided.
     '''
+    print ("Building initial network\n")
     N = nx.Graph()
     for s in scoredict:
-        if scoredict[s] >= args.threshold:
-            v1, v2 = s.split("_")
-            if v1 not in N:
-                N.add_node(v1, host=hostdict[v1])
-            if v2 not in N:
-                N.add_node(v2, host=hostdict[v2])
-            N.add_edge(v1, v2, weight=scoredict[s])
+        v1, v2 = s.split("|")
+        if v1 not in N:
+            N.add_node(v1, host=hostdict[v1])
+        if v2 not in N:
+            N.add_node(v2, host=hostdict[v2])
+        N.add_edge(v1, v2, weight=scoredict[s])
+    print ("Network built with %s nodes and %s edges" % (len(N.nodes()), len(N.edges())))
     return (N)
 
 
@@ -53,20 +55,33 @@ def plotNetwork(N, args, colourdict, outfile, lws=1):
     '''
     Draws a diagram of the network.
     '''
+    print ("Plotting\n")
     f = plt.figure(figsize=(8, 8))
     a = f.add_subplot('111')
     colours = []
+    hosts = []
     for n in N.nodes(data=True):
         colours.append(colourdict[n[1]['host']])
+        hosts.append(n[1]['host'])
+    hosts = set(hosts)
+    patches = []
+    labels = []
+    for host in hosts:
+        labels.append(host)
+        patches.append(mpatches.Patch(color=colourdict[host]))
     if args.shuffle:
         positions = shufflePositions(N, args.shuffle)
         nx.draw_networkx(N, ax=a, node_color=colours, pos=positions,
-                         niter=args.niter, k=args.k, linewidths=lws)
+                         niter=args.niter, k=args.k, linewidths=lws,
+                         with_labels=False)
+    
     else:
         nx.draw_networkx(N, ax=a, node_color=colours,
-                         niter=args.niter, k=args.k, linewidths=lws)
+                         niter=args.niter, k=args.k, linewidths=lws,
+                         with_labels=False)
     a.collections[0].set_edgecolor("#000000") 
     a.axis('off')
+    a.legend(handles=patches, labels=labels)
     f.savefig(outfile)
 
 
@@ -76,7 +91,10 @@ def runConnectedComponents(N, args, colourdict, hostdict):
     '''
     ccs = nx.connected_component_subgraphs(N)
     i = 1
+    print ("Analysing %s connected components\n" % len(list(ccs)))
+    ccs = nx.connected_component_subgraphs(N)
     for cc in ccs:
+        print ("Plotting connected component %i" % i)
         out = open("node_stats_%s.tsv" % i, "w")
         paths = nx.algorithms.shortest_paths.unweighted.all_pairs_shortest_path(N)
         dc = nx.degree_centrality(cc)
@@ -95,7 +113,6 @@ def runConnectedComponents(N, args, colourdict, hostdict):
         plotNetwork(cc, args, colourdict, "%s.png" % i, lws=lws)
         out = open("shortest_paths.tsv", "w")
         for path in paths:
-
             hosts = []
             for p in paths[path]:
                 a_to_b = paths[path][p]
@@ -110,24 +127,49 @@ def runConnectedComponents(N, args, colourdict, hostdict):
         out.close()
         i += 1
 
-    
+def getColours():
+    return (['#e6194b', '#3cb44b', '#ffe119', '#0082c8',
+             '#f58231', '#911eb4', '#46f0f0', '#f032e6',
+             '#d2f53c', '#fabebe', '#008080', '#e6beff',
+             '#aa6e28', '#fffac8', '#800000', '#aaffc3',
+             '#808000', '#ffd8b1', '#000080', '#808080',
+             '#FFFFFF', '#000000'])
+
+def randomColour():
+    colour = '#{:02x}{:02x}{:02x}'.format(*map(
+        lambda x: random.randint(0, 255), range(3)))
+    return (colour)
+
 def parseInput(args):
     '''
     Convert the input files into dictionaries.
     '''
-    similarity = [line.strip().split("\t") for line in open(
-        args.similarityfile).readlines()]
+    print ("Importing distance matrix\n")
     scoredict = dict()
-    for s in similarity:
-        scoredict["%s_%s" % (s[0], s[1])] = float(s[2])
-
-    hosts = [line.strip().split("\t") for line in open(
-        args.hostfile).readlines()]
-    hostdict = dict(hosts)
-
-    colourdict = dict([line.strip().split("\t")
-                       for line in open(args.colours).readlines()])
-
+    hostdict = dict()
+    familydict = dict([line.split("\t")[0:2] for line in open(args.hostfile).readlines()])
+    with open(args.similarityfile) as similarity:
+        for s in similarity:
+            s = s.split("\t")
+            if float(s[2]) >= args.threshold:
+                host1 = s[0][0:4]
+                host2 = s[1][0:4]
+                family1 = familydict[host1]
+                family2 = familydict[host2]
+                one = (family1 != family2 and args.transtype == "crossfamily")
+                two = (host1 != host2 and args.transtype == "crosshost")
+                three = (args.transtype == "all")
+                if (one or two or three):
+                    scoredict["%s|%s" % (s[0], s[1])] = float(s[2])
+                    hostdict[s[0]] = family1
+                    hostdict[s[1]] = family2
+    print ("Imported %i datapoints from distance matrix" % len(scoredict))
+    colours = getColours()
+    colourdict = dict()
+    i = 0
+    for host in familydict:
+        colourdict[familydict[host]] = randomColour()
+        i += 1
     return (hostdict, scoredict, colourdict)
 
 
@@ -146,6 +188,8 @@ if __name__ == "__main__":
                         default=0.1)
     parser.add_argument('--niter', dest='niter', type=int,
                         default=1000)
+    parser.add_argument('--transtype', dest='transtype', type=str,
+                        default='crossfamily')
     args = parser.parse_args()
     hostdict, scoredict, colourdict = parseInput(args)
 
